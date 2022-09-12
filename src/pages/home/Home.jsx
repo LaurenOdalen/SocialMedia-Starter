ff0da47bd8cc04b85c8f40c9bd263b46270721d2
LaurenOdalen <laurenodalen@gmail.com>
2022-09-12 10:33:10 -0600

    new commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+import ProfileSide from '../../Components/profileSide/ProfileSide'
+
+const Home = () => {
+    return (
+    <div className='Home'>
+        <ProfileSide/>
+        <div className='postSide'>Post</div>
+        <div className='profileSide'>Rightside</div>
+    </div>
+    )
+}
+
+export default Home
